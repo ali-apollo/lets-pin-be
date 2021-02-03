aaf87815d18ed2dc68595d7c0d4c62e244b976b5
FuzzyFade <1339184537@qq.com>
2021-02-03 13:33:08 +0800

    bug: 修复user service bug

--- app/service/user.ts
+++ app/service/user.ts
@@ -1,48 +1,34 @@
 import { Service } from 'egg';
 
 export default class User extends Service {
-  public async checkLogin(username, password) {
-    try {
-      const ret = this.verifyPassword(username, password)
-      return ret
-    } catch (error) {
-      throw error
-    }
+  public async checkLogin(username: string, password: string) {
+    await this.verifyPassword(username, password);
   }
 
-  public async doSignup(username, password) {
-    try {
-      return this.checkSignup(username, password)
-    } catch (error) {
-      throw error
-    }
+  public async doSignup(username: string, password: string) {
+    await this.checkSignup(username, password);
   }
 
-  async verifyPassword(username, password) {
-    console.log(username, password)
-    const isUserExit = await this.ctx.model.User.findOne({ username })
-    if(!isUserExit) throw '用户不存在'
-    
-    const ret = await this.ctx.model.User.findOne({ username, password })
-    if(!ret) throw '账号或密码错误'
-    return true
+  async verifyPassword(username: string, password: string) {
+    console.log(username, password);
+    const isUserExit = await this.ctx.model.User.findOne({ username });
+    if (!isUserExit) throw '用户不存在';
+
+    const ret = await this.ctx.model.User.findOne({ username, password });
+    if (!ret) throw '账号或密码错误';
   }
 
-  async checkSignup(username, password) {
-    console.log(username, password)
-    const isUserExit = await this.ctx.model.User.findOne({ username })
-    if(isUserExit) throw '用户已存在'
+  async checkSignup(username: string, password: string) {
+    console.log(username, password);
+    const isUserExit = await this.ctx.model.User.findOne({ username });
+
+    if (isUserExit) throw '用户已存在';
 
     const user = {
       userId: username,
       username,
-      password
-    }
-    try {
-      this.ctx.model.User.create(user)
-      return true
-    } catch (error) {
-      throw error
-    }
+      password,
+    };
+    await this.ctx.model.User.create(user);
   }
 }
